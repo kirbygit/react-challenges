11079c382b959c1e7d738d472454d797297c0352
kboygit <heybisayangdako@gmail.com>
2017-07-03 18:50:11 -0700

    update challenges

---
+++ app/stateless-components.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+const Repos = React.createClass ({
+  render() {
+    return (
+      <div>
+        <h3> User Repos </h3>
+        <ul>
+      </div>
+    );
+  }
+})
